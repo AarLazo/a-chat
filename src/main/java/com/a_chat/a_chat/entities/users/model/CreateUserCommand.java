4d06598b394ad6d531f65545207356ac25ce2cca
AarLazo <aar.lazo.dev@gmail.com>
2025-06-10 00:31:24 -0500

    added: - CreateUserCommand

---
+++ src/main/java/com/a_chat/a_chat/entities/users/model/CreateUserCommand.java
@@ -0,0 +1,18 @@
+package com.a_chat.a_chat.entities.users.model;
+
+import lombok.Getter;
+
+@Getter
+public class CreateUserCommand {
+    private String username;
+    private String email;
+    private String password;
+    private String birthdate;
+
+    public CreateUserCommand(String username, String email, String password, String birthdate) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.birthdate = birthdate;
+    }
+}
